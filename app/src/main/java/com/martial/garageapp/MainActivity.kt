a3543468f255225eb5942329f3349a4ce104dd32
martial <surajkumarsahani1997@gmail.com>
2022-08-24 15:27:22 +0530

    UI polish

--- app/src/main/java/com/martial/garageapp/MainActivity.kt
+++ app/src/main/java/com/martial/garageapp/MainActivity.kt
@@ -34,6 +34,7 @@ class MainActivity : AppCompatActivity()  {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        supportActionBar?.hide()
         initialize()
         onclick()
         //volley()
--- app/src/main/java/com/martial/garageapp/SignupActivity.kt
+++ app/src/main/java/com/martial/garageapp/SignupActivity.kt
@@ -19,6 +19,7 @@ class SignupActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signup)
+        supportActionBar?.hide()
         initialize()
         onclick()
     }
